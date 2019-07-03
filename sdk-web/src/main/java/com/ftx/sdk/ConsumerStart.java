965c438ee75919fe541c7571f3a8159e2d6348f8
vivianbackslide <834543320@qq.com>
2019-07-03 19:13:12 +0800

    web修改为war启动

--- sdk-web/src/main/java/com/ftx/sdk/ConsumerStart.java
+++ sdk-web/src/main/java/com/ftx/sdk/ConsumerStart.java
@@ -2,6 +2,10 @@ package com.ftx.sdk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 描述：主函数入口
@@ -9,10 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @auth:xiaojun.yin
  * @createTime 2019-04-06 14:57
  */
-@SpringBootApplication
-public class ConsumerStart {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+public class ConsumerStart extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerStart.class);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConsumerStart.class);
     }
 }
