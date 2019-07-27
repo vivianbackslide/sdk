b8c24c686650148e172c3ca483cf49363bc4f67b
vivianbackslide <834543320@qq.com>
2019-07-27 14:58:37 +0800

    增加sample2

---
+++ sdk-interface/src/main/java/com/ftx/sdk/login/SampleLoginHandler2.java
@@ -0,0 +1,50 @@
+package com.ftx.sdk.login;
+
+import com.ftx.sdk.annotation.ChannelAnnotation;
+import com.ftx.sdk.entity.sdk.SdkParamCache;
+import com.ftx.sdk.entity.user.LoginInfo;
+import com.ftx.sdk.entity.user.User;
+import org.apache.http.util.TextUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@ChannelAnnotation(channelId = 10100, channelLabel = "sample", channelName = "模拟渠道")
+public class SampleLoginHandler2 extends LoginHandlerAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(SampleLoginHandler2.class);
+
+    @Override
+    protected User doLogin(SdkParamCache configCache, LoginInfo loginInfo) {
+
+        //获得该包关于渠道的配置
+        String channelAppId = configCache.channelConfig().get("channelAppId");
+        String channelAppKey = configCache.channelConfig().get("channelAppKey");
+        String channelAppSecret = configCache.channelConfig().get("channelAppSecret");
+        logger.debug(String.format("%s %s %s", channelAppId, channelAppKey, channelAppSecret));
+
+        //客户端上传的用户信息在 loginInfo
+//		loginInfo.getToken();  //这个必须不为空
+//		loginInfo.getUserId(); //这个有可能为空
+//		loginInfo.getExInfo(); //这个是额外的信息
+
+        String sampleUserId = loginInfo.getUserId();
+        String userId;
+        if ("test_123456".equals(sampleUserId) && "token_123456_特殊字符测试+ /?%#&=".equals(loginInfo.getToken())) {
+            userId = "test_123";
+        } else if ("test_654321".equals(sampleUserId) && "token_654321".equals(loginInfo.getToken())) {
+            userId = "test_456";
+        } else if (TextUtils.isEmpty(sampleUserId) && "token_123".equals(loginInfo.getToken())) {
+            userId = "test_321";
+        } else {
+            return null;
+        }
+
+        User user = new User();
+        user.setUserId(userId);
+        user.setChannelId(configCache.getChannelId());
+        user.setChannelName(configCache.getChannelLabel());
+        return user;
+    }
+}
